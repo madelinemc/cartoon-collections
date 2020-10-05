766e71f9978c28919387bd8b09860769e4bf1b78
madelinemc <madeline.coons@gmail.com>
2020-10-05 16:28:30 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array_of_names)
+  i = 0
+  roll_call = []
+  while i < array_of_names.length 
+    array_of_names.each_with_index { |name, spot_in_line| 
+    roll_call << "#{spot_in_line + 1}. #{name}"}
+    i += 1
+  end
+  puts roll_call
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map { |string| string.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array_of_calls = []
+  array_of_calls << array.count
+  array_of_calls.any?{|call_count| call_count <= 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  if array.include?("cheddar")
+    "cheddar"
+  elsif array.include?("gouda")
+    "gouda"
+  elsif array.include?("camembert")
+    "camembert"
+  else 
+  end
+end
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  #cheese_types = ["cheddar", "gouda", "camembert"]
